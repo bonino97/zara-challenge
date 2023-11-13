66a3926add62e9cb0c5dd13ec97804e4046d4441
bonino97 <juanbonino97@gmail.com>
2023-11-13 13:53:41 +0100

    Add zoom logic

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,6 +5,8 @@ import {
   Draggable,
   OnDragEndResponder,
 } from 'react-beautiful-dnd';
+import { Button } from '@mui/material';
+import { PlusIcon, ZoomInIcon, ZoomOutIcon } from '@radix-ui/react-icons';
 
 import useAxios from '#/hooks/useAxios';
 import { IProduct } from '#/interfaces/IProduct';
@@ -14,12 +16,17 @@ import Card from '#/components/Card';
 const HomePage: React.FC = () => {
   const axios = useAxios();
   const [rows, setRows] = useState<IProduct[][]>([]);
+  const [zoomLevel, setZoomLevel] = useState<number>(1);
+  const [isDragging, setIsDragging] = useState<boolean>(true);
 
   const getProducts = async () => {
     try {
       const { data, error } = await axios.get('/products');
+
       if (error) return alert(`Error: ${error}`);
+
       const newRows: IProduct[][] = [[], [], []];
+
       for (let i = 0; i < data.length; i++) {
         const rowIndex = i % 3; // Esto distribuirá los productos en tres filas
         if (newRows[rowIndex].length < 3) {
@@ -33,7 +40,12 @@ const HomePage: React.FC = () => {
     }
   };
 
-  const handleOnDragEnd: OnDragEndResponder = (result) => {
+  const handleOnDragStart = () => {
+    setIsDragging(true); // Se inicia el arrastre
+  };
+
+  const handleOnDragEnd: OnDragEndResponder = async (result) => {
+    setIsDragging(false); // Se termina el arrastre
     const { source, destination } = result;
     // No hacer nada si no hay un destino o si el ítem se soltó en el mismo lugar
     if (
@@ -64,8 +76,21 @@ const HomePage: React.FC = () => {
       alert('No puedes añadir más de 3 ítems en una fila.');
     }
 
-    // Actualiza el estado con las nuevas filas
     setRows(newRows);
+  };
+
+  const addRow = () => {
+    const newRows = [...rows];
+    newRows.push([]);
+    setRows(newRows);
+  };
+
+  const zoomIn = () => {
+    setZoomLevel((prevZoom) => Math.min(prevZoom + 0.1, 2)); // Límite máximo de 2x zoom
+  };
+
+  const zoomOut = () => {
+    setZoomLevel((prevZoom) => Math.max(prevZoom - 0.1, 0.5)); // Límite mínimo de 0.5x zoom
   };
 
   useEffect(() => {
@@ -76,37 +101,71 @@ const HomePage: React.FC = () => {
     <div className="flex justify-center items-start min-h-screen bg-gray-100 p-5">
       <div className="container mx-auto">
         <Header />
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          {rows.map((row, rowIndex) => (
-            <Droppable
-              droppableId={`droppable-${rowIndex}`}
-              direction="horizontal"
-              key={rowIndex}
-              isCombineEnabled={false}
-            >
-              {(provided, snapshot) => (
-                <div
-                  ref={provided.innerRef}
-                  {...provided.droppableProps}
-                  className="flex justify-center space-x-4 mb-4 border-2 border-gray-300 rounded-md p-4"
-                >
-                  {row.map((product, productIndex) => (
-                    <Draggable
-                      key={product.id}
-                      draggableId={product.id}
-                      index={productIndex}
-                    >
-                      {(provided, snapshot) => (
-                        <Card provided={provided} product={product} />
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-          ))}
-        </DragDropContext>
+        <div className="flex justify-end py-4">
+          <Button variant="outlined" onClick={zoomOut} size="large">
+            <ZoomOutIcon className="w-4 h-4" />
+          </Button>
+          <Button variant="outlined" onClick={zoomIn} size="large">
+            <ZoomInIcon className="w-4 h-4" />
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={addRow}
+            size="large"
+            startIcon={<PlusIcon className="w-4 h-4" />}
+          >
+            Add Row
+          </Button>
+        </div>
+        <div
+          style={{
+            transform: isDragging ? 'none' : `scale(${zoomLevel})`,
+            transformOrigin: 'top center',
+            transition: 'transform 0.2s', // Agrega una transición suave al cambiar el zoom
+          }}
+        >
+          <DragDropContext
+            onDragStart={handleOnDragStart}
+            onDragEnd={handleOnDragEnd}
+          >
+            {rows.map((row, rowIndex) => (
+              <Droppable
+                droppableId={`droppable-${rowIndex}`}
+                direction="horizontal"
+                key={rowIndex}
+                isCombineEnabled={false}
+              >
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    className="flex justify-center space-x-4 mb-4 border-2 border-gray-300 rounded-md p-4"
+                    style={{
+                      backgroundColor: snapshot.isDraggingOver
+                        ? '#dfe6e9'
+                        : 'transparent',
+                      borderStyle: row.length === 0 ? 'dashed' : 'solid',
+                      minHeight: '200px',
+                    }}
+                  >
+                    {row.map((product, productIndex) => (
+                      <Draggable
+                        key={product.id}
+                        draggableId={product.id}
+                        index={productIndex}
+                      >
+                        {(provided, snapshot) => (
+                          <Card provided={provided} product={product} />
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            ))}
+          </DragDropContext>
+        </div>
       </div>
     </div>
   );
